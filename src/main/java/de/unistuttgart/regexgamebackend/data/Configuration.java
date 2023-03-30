e2642f0d413da7f0364c283f87192b3d67895462
programminghoch10 <16062290+programminghoch10@users.noreply.github.com>
2023-03-30 11:35:58 +0200

    remove game configuration options complexity and answerCount

--- src/main/java/de/unistuttgart/regexgamebackend/data/Configuration.java
+++ src/main/java/de/unistuttgart/regexgamebackend/data/Configuration.java
@@ -3,13 +3,12 @@ package de.unistuttgart.regexgamebackend.data;
 import java.util.EnumSet;
 import java.util.UUID;
 import javax.persistence.*;
-import javax.validation.Valid;
+import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 import lombok.AccessLevel;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.FieldDefaults;
-import org.hibernate.validator.constraints.Range;
 import org.springframework.validation.annotation.Validated;
 
 /**
@@ -33,26 +32,7 @@ public class Configuration {
      * Allowed regex structures for this game.
      */
     @Size(min = 1)
-    EnumSet<RegexStructure> allowRegexStructures;
+    @NotNull
+    EnumSet<RegexStructure> allowedRegexStructures;
 
-    /**
-     * Game complexity factor.
-     */
-    @Range(min = 1)
-    @Valid
-    double complexity = 1;
-
-    /**
-     * Whether the complexity of the game should gradually increase with each new
-     * challenge.
-     */
-    @Valid
-    boolean increaseComplexity = true;
-
-    /**
-     * The amount of answers to be generated.
-     */
-    @Range(min = 2, max = 9)
-    @Valid
-    int answerCount = 3;
 }
