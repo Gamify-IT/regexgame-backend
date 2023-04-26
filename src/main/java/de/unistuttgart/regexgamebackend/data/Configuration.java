4a2590dca1d5a3080e326d6b7c5e8bc475d047a6
programminghoch10 <16062290+programminghoch10@users.noreply.github.com>
2023-04-26 17:09:35 +0200

    import game configuration parameters from frontend

--- src/main/java/de/unistuttgart/regexgamebackend/data/Configuration.java
+++ src/main/java/de/unistuttgart/regexgamebackend/data/Configuration.java
@@ -2,14 +2,21 @@ package de.unistuttgart.regexgamebackend.data;
 
 import java.util.EnumSet;
 import java.util.UUID;
-import javax.persistence.*;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+
+import org.hibernate.validator.constraints.Range;
+import org.springframework.lang.Nullable;
+import org.springframework.validation.annotation.Validated;
+
 import lombok.AccessLevel;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import lombok.experimental.FieldDefaults;
-import org.springframework.validation.annotation.Validated;
 
 /**
  * Contains all data that have to be stored to configure a regex game.
@@ -35,4 +42,28 @@ public class Configuration {
     @NotNull
     EnumSet<RegexStructure> allowedRegexStructures;
 
+    /**
+     * minimum required rounds for the game to be counted as completed
+     * if null the default defined in the frontend will be used
+     */
+    @Nullable
+    Integer minimumCompletedRounds;
+
+    /**
+     * how many answers to show in each riddle
+     * if null the default defined in the frontend will be used
+     */
+    @Nullable
+    Integer answerCount;
+
+    /**
+     * how many seconds the user has to solve a given riddle
+     * the timeout will accumulate over the riddles,
+     * so the remaining time will be carried over to the next riddle
+     * if zero there is no timeout
+     * if null the default defined in the frontend will be used
+     */
+    @Nullable
+    @Range(min = 0)
+    Integer riddleTimeoutSeconds;
 }
