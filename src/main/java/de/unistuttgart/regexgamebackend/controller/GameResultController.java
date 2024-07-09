f6ea0f81fbbcf619880310095eac3b03470f4ba5
Fabian <fabian.kalitzke@arcor.de>
2024-07-09 12:38:24 +0200

    Added reward calculation

--- src/main/java/de/unistuttgart/regexgamebackend/controller/GameResultController.java
+++ src/main/java/de/unistuttgart/regexgamebackend/controller/GameResultController.java
@@ -6,11 +6,7 @@ import de.unistuttgart.regexgamebackend.service.GameResultService;
 import io.swagger.v3.oas.annotations.parameters.RequestBody;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.CookieValue;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/results")
@@ -24,12 +20,25 @@ public class GameResultController {
 
     @PostMapping("")
     @ResponseStatus(HttpStatus.OK)
-    public void saveGameResult(
+    public GameResult saveGameResult(
         @CookieValue("access_token") final String accessToken,
         @RequestBody final GameResult gameResult
     ) {
         jwtValidatorService.validateTokenOrThrow(accessToken);
         gameResult.setUserId(jwtValidatorService.extractUserId(accessToken));
+        gameResult.setRewards(this.calculateRewards(gameResult.getScore()));
         gameResultService.submitGameResult(gameResult, accessToken);
+        return gameResult;
+    }
+
+    /**
+     * Calculates the amount of rewards given to the player.
+     * The amount changes depending on the score, with a successful run giving 10 coins.
+     *
+     * @param score completion in percent
+     * @return rewards (range 0 - 10)
+     */
+    private int calculateRewards(final long score) {
+        return (int) score/10;
     }
 }
